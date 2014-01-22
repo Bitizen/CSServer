49b4726bb07a99cf4afcfe98252a3b073421d5a8
Kashka Enverga <kdp.enverga@gmail.com>
2014-01-22 13:07:30 +0800

    no message

--- src/com/bitizen/server/GameLogic.java
+++ src/com/bitizen/server/GameLogic.java
@@ -1,12 +1,55 @@
 package com.bitizen.server;
 
+
 public class GameLogic {
+	private static final int LoginUserName = 0;
+	private static final int LoginPassword = 1;
+	private static final int AuthenticateUser = 2;
+	private static final int AuthSuccess   = 3;
+
+	private int state = LoginUserName;
+
+	private String userName =  null;
+
+	public String processInput(String clientRequest) {
+		String reply = null;
+		try {
+			if(clientRequest != null && clientRequest.equalsIgnoreCase("login")) {
+				state = LoginPassword;
+			}if(clientRequest != null && clientRequest.equalsIgnoreCase("exit")) {
+				return "exit";
+			}
+
+			if(state == LoginUserName) {
+				reply = "username: ";
+				state = AuthenticateUser;
+			} else if(state == AuthenticateUser) {
+				userName = clientRequest;
+				
+				if(userName.equalsIgnoreCase("kashka")) { 
+					reply = "success";
+					state = AuthSuccess;
+				} else {
+					reply = "taken. username:";
+					state = LoginUserName;
+				}
+			} else {
+				reply = "invalid";
+			}
+		} catch(Exception e) {
+			System.out.println("input process falied: " + e.getMessage());
+			return "exit";
+		}
+
+		return reply;
+	}
+}
 	/*
 	private static final int LoginUserName = 0;
 	private static final int LoginPassword = 1;
 	private static final int AuthenticateUser = 2;
 	private static final int AuthSuccess   = 3;
-	 */
+	 
 	
 	private static final int LoginUserName = 0;			//Login and Verify available username
 	private static final int ViewMatches = 1;			//View Matches, Select Match, Verify if not full
@@ -19,6 +62,8 @@ public class GameLogic {
 	private String userMatch =  null;
 	private String userTeam =  null;
 
+	
+	
 	public String processInput(String clientRequest) {
 		String reply = null;
 		try {
@@ -32,6 +77,7 @@ public class GameLogic {
 			if(state == LoginUserName) {
 				userName = clientRequest;
 				if(userName.equalsIgnoreCase("Kashka")) { 
+				// ask webserver if username is taken (via php, JSONParser)
 					reply = "Login success.";
 					state = ViewMatches;
 				} else {
@@ -39,7 +85,7 @@ public class GameLogic {
 				}
 			} else if(state == ViewMatches) {
 				// Output matches
-				
+				// ask webserver for list of hosts
 				// Choose match, CHECK IF FULL
 				userMatch = clientRequest;
 				if(userMatch.equalsIgnoreCase("Dice")) { 
@@ -65,4 +111,5 @@ public class GameLogic {
 
 		return reply;
 	}
-}
+	}
+	*/
